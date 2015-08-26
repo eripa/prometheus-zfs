a245aaca7e9511211d2e33f4b585e58f3928562f
Eric Ripa <eric@ripa.io>
2015-08-26 16:40:33 +0200

    Heavily modified to be a local exporter to Prometheus text format (not done)

--- prometheus-zfs.go
+++ prometheus-zfs.go
@@ -4,92 +4,62 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"sync"
-
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 const (
-	toolVersion = "0.1.0"
+	toolVersion   = "0.2.0"
+	capacityLabel = "zpool_capacity_percentage"
+	capacityHelp  = "Current zpool capacity level"
+	faultedLabel  = "zpool_faulted_providers_count"
+	faultedHelp   = "Number of FAULTED/UNAVAIL zpool providers (disks)"
+	onlineLabel   = "zpool_online_providers_count"
+	onlineHelp    = "Number of ONLINE zpool providers (disks)"
 )
 
-// Exporter collects zpool stats from the given zpool and exports them using
-// the prometheus metrics package.
-type Exporter struct {
-	mutex sync.RWMutex
-
-	poolUsage, providersFaulted, providersOnline prometheus.Gauge
-	zpool                                        *zpool
+type exporter struct {
+	zpool *zpool
 }
 
-// NewExporter returns an initialized Exporter.
-func NewExporter(zp *zpool) *Exporter {
-	// Init and return our exporter.
-	return &Exporter{
-		zpool: zp,
-		poolUsage: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "zpool_capacity_percentage",
-			Help: "Current zpool capacity level",
-		}),
-		providersOnline: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "zpool_online_providers_count",
-			Help: "Number of ONLINE zpool providers (disks)",
-		}),
-		providersFaulted: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "zpool_faulted_providers_count",
-			Help: "Number of FAULTED/UNAVAIL zpool providers (disks)",
-		}),
-	}
-}
-
-// Describe describes all the metrics ever exported by the zpool exporter. It
-// implements prometheus.Collector.
-func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
-	ch <- e.poolUsage.Desc()
-	ch <- e.providersOnline.Desc()
-	ch <- e.providersFaulted.Desc()
-}
-
-// Collect fetches the stats from configured ZFS pool and delivers them
-// as Prometheus metrics. It implements prometheus.Collector.
-func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-
-	e.mutex.Lock() // To protect metrics from concurrent collects.
-	defer e.mutex.Unlock()
-
+func (e *exporter) export() {
 	e.zpool.getStatus()
-	e.poolUsage.Set(float64(e.zpool.capacity))
-	e.providersOnline.Set(float64(e.zpool.online))
-	e.providersFaulted.Set(float64(e.zpool.faulted))
+	e.writeUsage()
+	e.writeOnline()
+	e.writeFaulted()
+}
 
-	ch <- e.poolUsage
-	ch <- e.providersOnline
-	ch <- e.providersFaulted
+func (e *exporter) writeUsage() {
+	fmt.Printf("# HELP %s %s\n", capacityLabel, capacityHelp)
+	fmt.Printf("# TYPE %s gauge\n", capacityLabel)
+	fmt.Printf("%s %d\n", capacityLabel, e.zpool.capacity)
+}
+
+func (e *exporter) writeFaulted() {
+	fmt.Printf("# HELP %s %s\n", faultedLabel, faultedHelp)
+	fmt.Printf("# TYPE %s gauge\n", faultedLabel)
+	fmt.Printf("%s %d\n", faultedLabel, e.zpool.faulted)
+}
+
+func (e *exporter) writeOnline() {
+	fmt.Printf("# HELP %s %s\n", onlineLabel, onlineHelp)
+	fmt.Printf("# TYPE %s gauge\n", onlineLabel)
+	fmt.Printf("%s %d\n", onlineLabel, e.zpool.online)
 }
 
 var (
-	zfsPool       string
-	listenPort    string
-	metricsHandle string
-	versionCheck  bool
+	zfsPool      string
+	versionCheck bool
 )
 
 func init() {
 	const (
-		defaultPool   = "tank"
-		selectedPool  = "what ZFS pool to monitor"
-		versionUsage  = "display current tool version"
-		defaultPort   = "8080"
-		portUsage     = "Port to listen on"
-		defaultHandle = "metrics"
-		handleUsage   = "HTTP endpoint to export data on"
+		defaultPool  = "tank"
+		selectedPool = "what ZFS pool to monitor"
+		versionUsage = "display current tool version"
+		handleUsage  = "HTTP endpoint to export data on"
 	)
 	flag.StringVar(&zfsPool, "pool", defaultPool, selectedPool)
 	flag.StringVar(&zfsPool, "p", defaultPool, selectedPool+" (shorthand)")
-	flag.StringVar(&listenPort, "port", defaultPort, portUsage)
-	flag.StringVar(&metricsHandle, "endpoint", defaultHandle, handleUsage)
 	flag.BoolVar(&versionCheck, "version", false, versionUsage)
 	flag.Parse()
 }
@@ -99,18 +69,11 @@ func main() {
 		fmt.Printf("prometheus-zfs v%s (https://github.com/eripa/prometheus-zfs)\n", toolVersion)
 		os.Exit(0)
 	}
-	err := checkExistance(zfsPool)
+	err := checkExistence(zfsPool)
 	if err != nil {
 		log.Fatal(err)
 	}
 	z := zpool{name: zfsPool}
-	z.getStatus()
-
-	exporter := NewExporter(&z)
-	prometheus.MustRegister(exporter)
-
-	fmt.Printf("Starting zpool metrics exporter on :%s/%s\n", listenPort, metricsHandle)
-	http.Handle("/"+metricsHandle, prometheus.Handler())
-	http.ListenAndServe(":"+listenPort, nil)
-
+	exporter := exporter{zpool: &z}
+	exporter.export()
 }
